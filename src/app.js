84ecc47981207bb001288b488a694551d1a27897
DalexisHub <alexisdiego478@gmail.com>
2026-05-09 09:39:37 -0500

    Initial Express MongoDB Atlas API setup

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API Express MongoDB Atlas funcionando correctamente'
+  });
+});
+
+app.use('/api/users', userRoutes);
+
+export default app;
